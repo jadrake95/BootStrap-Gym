495b8ab1234ff2884805a83d3ec85e6580fea8e2
Jack Drake <jadrake95@gmail.com>
2021-02-21 23:34:15 +0900

    fixed typo in imgs src

--- public/js/app.js
+++ public/js/app.js
@@ -6,12 +6,12 @@ window.addEventListener("scroll", function () {
     navbar.classList.add("navbar-light");
     navbar.classList.remove("bg-transparent");
     navbar.classList.remove("navbar-dark");
-    logo.src = "/public/images/NEW_LOGO.svg";
+    logo.src = "public/images/NEW_LOGO.svg";
   } else {
     navbar.classList.remove("bg-light");
     navbar.classList.remove("navbar-light");
     navbar.classList.add("bg-transparent");
     navbar.classList.add("navbar-dark");
-    logo.src = "/public/images/NEW_LOGO(light).svg";
+    logo.src = "public/images/NEW_LOGO(light).svg";
   }
 });

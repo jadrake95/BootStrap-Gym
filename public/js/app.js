27d4be776fb28714d7d735b9a85269834a45904e
Jack Drake <jadrake95@gmail.com>
2021-02-21 23:27:36 +0900

    added new rounded images for profiles, worked on teams layout and responsiveness

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector("#homeNav");
+  const logo = document.querySelector("#mainLogo");
+  if (window.scrollY > navbar.offsetTop + navbar.offsetHeight) {
+    navbar.classList.add("bg-light");
+    navbar.classList.add("navbar-light");
+    navbar.classList.remove("bg-transparent");
+    navbar.classList.remove("navbar-dark");
+    logo.src = "/public/images/NEW_LOGO.svg";
+  } else {
+    navbar.classList.remove("bg-light");
+    navbar.classList.remove("navbar-light");
+    navbar.classList.add("bg-transparent");
+    navbar.classList.add("navbar-dark");
+    logo.src = "/public/images/NEW_LOGO(light).svg";
+  }
+});
